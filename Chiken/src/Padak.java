9f618634b6c5ae78563635cfdc4b95828d6d4a05
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2022-04-14 21:10:10 +0900

    교촌 치킨을 상속 받아 파닭을 만드는 클래스를 구현하라. 실행하면 다음과 같이 출력하면 된다. $ java Main Timer: 1분 Timer: 2분 Timer: 3분 Timer: 4분 Timer: 5분 다 익었습니다. 파를 100그램 얹는다. $

---
+++ Chiken/src/Padak.java
@@ -0,0 +1,10 @@
+
+public class Padak{
+	private static KyoChon chick = new KyoChon();
+	public void MakeChicken() throws Exception {
+		
+		chick.MakeChicken();
+		System.out.println("파를 100그램 얹는다.");
+		
+	}
+}
